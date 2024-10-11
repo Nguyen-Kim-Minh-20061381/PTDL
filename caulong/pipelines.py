8074c78ff37c9222934d37c5c48adbe6d61fbfc2
kim minh <118244379+Nguyen-Kim-Minh-20061381@users.noreply.github.com>
2024-10-11 20:07:45 +0700

    Update pipelines.py

--- caulong/pipelines.py
+++ caulong/pipelines.py
@@ -16,7 +16,7 @@ import os
 
 class MongoDBCauLongPipeline:
     def __init__(self):
-        self.client = pymongo.MongoClient('mongodb://192.168.250.146:27017')
+        self.client = pymongo.MongoClient('mongodb://host.docker.internal:27017')
         self.db = self.client['dbcaulong'] #Create Database      
         pass
     def process_item(self, item, spider):
